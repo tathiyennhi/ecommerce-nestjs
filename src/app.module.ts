f7cb20d6d84eea440f5a5a6a10cc6641c8908026
ngoccao <ngoccao@phs.vn>
2024-05-23 16:50:14 +0700

    init db code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './connection/database/entities/Products';
+import { ProductsModule } from './business/products/products.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'test',
+      entities: [Product],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
