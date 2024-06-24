5b5f15c189a70827657285790a4a1df792d8531a
ngoccao <caonhungoc1996.com>
2024-06-24 21:56:48 +0700

    lưu tạm seeder

--- seed/init.ts
+++
@@ -1,3 +0,0 @@
-// await queryRunner.query(`INSERT INTO menu (id, name) VALUES ('44d1a1b0-cb9b-4f5d-a10f-a35dde4ee4b7', 'Thời trang nữ')`);
-// await queryRunner.query(`INSERT INTO menu (id, name) VALUES ('341276c1-dbf6-4ed7-909f-b10adc39d028', 'Thời trang nam')`);
-// await queryRunner.query(`INSERT INTO menu (id, name) VALUES ('61571790-6aa2-46a5-9ef7-518c16672ef1', 'Giới thiệu')`);
---
+++ seed/seed.helper.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+import { seedData } from './seed-data';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from 'src/app.module';
+
+async function runSeeder() {
+  const app = await NestFactory.create(AppModule);
+  const dataSource = app.get(DataSource);
+  await seedData(dataSource);
+  await app.close();
+}
+runSeeder();
